600a8ec150759150b053073348998d2cf248d3b3
binotation <68807919+binotation@users.noreply.github.com>
2024-08-31 19:53:39 +1000

    Don't poll TXE, buffer received data

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,35 @@
 //! USART1 reads GPS data from GP-735T and sends it over USART2.
+//! USART2 reads input and toggles GPS ON/OFF if b'0'/b'1'.
 
 #![no_std]
 #![no_main]
 
 use cortex_m_rt::entry;
+use heapless::spsc::Queue;
 use panic_semihosting as _; // logs messages to the host stderr; requires a debugger
 use stm32l4::stm32l4x2::{self, interrupt};
 
 static mut USART1_PERIPHERAL: Option<stm32l4x2::USART1> = None;
 static mut USART2_PERIPHERAL: Option<stm32l4x2::USART2> = None;
 static mut GPIOA_PERIPHERAL: Option<stm32l4x2::GPIOA> = None;
+static mut BUFFER: Option<Queue<u16, 64>> = None;
 
+/// Queue received bytes and enable USART2 TXE interrupt. Ignore null bytes.
 #[interrupt]
 fn USART1() {
     let usart1 = unsafe { USART1_PERIPHERAL.as_mut() }.unwrap();
     let usart2 = unsafe { USART2_PERIPHERAL.as_mut() }.unwrap();
-    if usart1.isr.read().rxne().bit_is_set() {
-        // Read off USART1 RX, this clears RXNE flag
-        let received_byte = usart1.rdr.read().rdr().bits();
+    let buffer = unsafe { BUFFER.as_mut() }.unwrap();
 
+    if usart1.isr.read().rxne().bit_is_set() {
+        // Read off USART1, this clears RXNE flag
+        let received_byte = usart1.rdr.read().rdr().bits();
         if received_byte != 0 {
-            // Write to USART2 TX
-            while usart2.isr.read().txe().bit_is_clear() {} // Poll TXE, should already be set
-            usart2.tdr.write(|w| w.tdr().bits(received_byte));
+            // Queue byte, do nothing if queue is full
+            if buffer.enqueue(received_byte).is_ok() {
+                // Enable USART2 TXE interrupt as buffer is now non-empty
+                usart2.cr1.modify(|_, w| w.txeie().enabled());
+            }
         }
     }
     // See reference manual p.1206 or ch. 38.7.
@@ -37,8 +44,20 @@ fn USART1() {
 fn USART2() {
     let usart2 = unsafe { USART2_PERIPHERAL.as_mut() }.unwrap();
     let gpioa = unsafe { GPIOA_PERIPHERAL.as_mut() }.unwrap();
+    let buffer = unsafe { BUFFER.as_mut() }.unwrap();
+
+    if usart2.isr.read().txe().bit_is_set() {
+        match buffer.dequeue() {
+            // Write dequeued byte
+            Some(byte) => usart2.tdr.write(|w| w.tdr().bits(byte)),
+            // Buffer is empty, disable USART2 TXE interrupt
+            None => usart2.cr1.modify(|_, w| w.txeie().disabled()),
+        }
+    }
+
+    // Received command from UART adaptor - toggle GPS ON/OFF
     if usart2.isr.read().rxne().bit_is_set() {
-        // Read off USART2 RX, this clears RXNE flag
+        // Read off USART2, this clears RXNE flag
         let received_byte = usart2.rdr.read().rdr().bits();
 
         // Turn off if '0', turn on if '1'
@@ -96,10 +115,12 @@ fn main() -> ! {
     dp.USART1.brr.write(|w| w.brr().bits(417)); // 4Mhz / 9600 approx. 417
     dp.USART2.brr.write(|w| w.brr().bits(417)); // 4Mhz / 9600 approx. 417
 
-    // Enable USART, transmitter, receiver and RXNE interrupt
+    // USART1 interfaces with GPS - enable receiver and RXNE interrupt
     dp.USART1
         .cr1
         .write(|w| w.re().enabled().ue().enabled().rxneie().enabled());
+    // USART2 interfaces with UART adaptor - enable receiver, transmitter and RXNE interrupt
+    // TXE interrupt is enabled by USART1 on demand
     dp.USART2.cr1.write(|w| {
         w.re()
             .enabled()
@@ -112,6 +133,7 @@ fn main() -> ! {
     });
 
     unsafe {
+        BUFFER = Some(Queue::default());
         // Unmask NVIC USART1, USART2 global interrupts
         cortex_m::peripheral::NVIC::unmask(stm32l4x2::Interrupt::USART1);
         cortex_m::peripheral::NVIC::unmask(stm32l4x2::Interrupt::USART2);
